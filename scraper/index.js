062dfdd3bd65610d2da3f86c0de7488f03057f6c
afaf98 <afaf.ibrahimi@gmail.com>
2021-01-12 09:17:33 +0100

    Refactor parse table function

--- scraper/index.js
+++ scraper/index.js
@@ -19,34 +19,34 @@ function getTablesSeasons(response) {
 }
 
 function getSeasons(tableSeasonElements) {
-  let seasons = [];
-  tableSeasonElements.forEach((season, index) => {
-    const rowCount = season.querySelectorAll("tr").length;
-    let headers = [];
-    let episodes = [];
-
-    for (let rowIndex = 0; rowIndex < rowCount; rowIndex++) {
-      let row = {};
-      var oCells = season.rows.item(rowIndex).cells;
-      var cellLength = oCells.length;
-      for (var cellIndex = 0; cellIndex < cellLength; cellIndex++) {
-        // get your cell info here
-        var cellVal = oCells.item(cellIndex).textContent;
-        if (rowIndex === 0) {
-          headers.push(cellVal);
-        } else {
-          row = { ...row, [headers[cellIndex]]: cellVal };
-        }
-      }
-      if (rowIndex != 0) {
-        episodes.push(row);
-      }
-    }
-
-    seasons.push({ season: index + 1, episodes: episodes });
-    console.log(seasons);
-    fs.writeFileSync("seasons.json", JSON.stringify(seasons));
-  });
+  let seasons = Array.from(tableSeasonElements).map(parseTable);
+  console.log(seasons);
+  fs.writeFileSync("seasons.json", JSON.stringify(seasons));
 }
 
 getHTML();
+function parseTable(season, index) {
+  const rowCount = season.querySelectorAll("tr").length;
+  let headers = [];
+  let episodes = [];
+
+  for (let rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+    let row = {};
+    var oCells = season.rows.item(rowIndex).cells;
+    var cellLength = oCells.length;
+    for (var cellIndex = 0; cellIndex < cellLength; cellIndex++) {
+      // get your cell info here
+      var cellVal = oCells.item(cellIndex).textContent;
+      if (rowIndex === 0) {
+        headers.push(cellVal);
+      } else {
+        row = { ...row, [headers[cellIndex]]: cellVal };
+      }
+    }
+    if (rowIndex != 0) {
+      episodes.push(row);
+    }
+  }
+
+  return { season: index + 1, episodes: episodes };
+}
