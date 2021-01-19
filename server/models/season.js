7bf22174513df5053ef89e207f3e88ad344f0902
afaf98 <afaf.ibrahimi@gmail.com>
2021-01-19 14:54:56 +0100

    setup express and create GET requests

--- server/models/season.js
+++ server/models/season.js
@@ -3,13 +3,11 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class season extends Model {
     static associate(models) {
-      season.hasMany(model.episode);
+      season.hasMany(models.episode);
     }
   }
   season.init(
-    {
-      nseason: DataTypes.INTEGER,
-    },
+    {},
     {
       sequelize,
       modelName: "season",
