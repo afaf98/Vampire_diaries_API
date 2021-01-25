c7c3892985af6a488515f02be9e1874bc97ce713
afaf98 <afaf.ibrahimi@gmail.com>
2021-01-25 15:14:39 +0100

    Compacting 2 routes and adding try/catch blocks

--- server/app.js
+++ server/app.js
@@ -9,55 +9,74 @@ app.use(cors());
 // Episode routes
 
 app.get("/episodes", async (req, res) => {
-  const episodes = await episode.findAll();
-  res.json(episodes);
+  try {
+    const episodes = await episode.findAll();
+    res.json(episodes);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal error" });
+  }
 });
 
 app.get("/episodes/:id", async (req, res) => {
-  const episodeById = await episode.findByPk(req.params.id);
+  try {
+    const episodeById = await episode.findByPk(req.params.id);
 
-  res.json(episodeById);
+    res.json(episodeById);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal error" });
+  }
 });
 
 //Season routes
 app.get("/seasons", async (req, res) => {
-  const seasons = await season.findAndCountAll();
-  res.json({ count: seasons.count, seasons: seasons.rows });
+  try {
+    const seasons = await season.findAndCountAll();
+
+    res.json({ count: seasons.count, seasons: seasons.rows });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal error" });
+  }
 });
 
 app.get("/seasons/:seasonId/episodes", async (req, res) => {
-  const episodes = await episode.findAll({
-    where: { seasonId: req.params.seasonId },
-  });
-  res.json(episodes);
+  try {
+    const episodes = await episode.findAll({
+      where: { seasonId: req.params.seasonId },
+    });
+    res.json(episodes);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal error" });
+  }
 });
 
 //Characters routes
 
 app.get("/characters", async (req, res) => {
-  const characters = await character.findAll({ attributes: ["character"] });
+  try {
+    const characters = await character.findAll();
 
-  res.json(characters);
+    res.json(characters);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal error" });
+  }
 });
 
 app.get("/actors", async (req, res) => {
-  const actors = await character.findAll({ attributes: ["actor"] });
+  try {
+    const actors = await character.findAll({
+      attributes: ["actor", "episodeCount"],
+    });
 
-  res.json(actors);
-});
-
-app.get("/actors/episodeCount", async (req, res) => {
-  const actors = await character.findAll({
-    attributes: ["actor", "episodeCount"],
-  });
-
-  res.json(actors);
-});
-
-app.get("/characters/actors/episodeCount", async (req, res) => {
-  const allCharacters = await character.findAll();
-
-  res.json(allCharacters);
+    res.json(actors);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal error" });
+  }
 });
 
 module.exports = app;
